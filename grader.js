47f47cf3fe62eace35133aece75e27ff5ea43435
Ubuntu <ubuntu@ip-172-31-25-112.us-west-2.compute.internal>
2013-07-05 01:45:35 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://localhost:5000";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,33 +39,81 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlFormat = function(inurl) {
+    var instr = inurl.toString();
+    if (instr.split(' ').length != 1) {
+	console.log("%s url is in invalid format. Use --url <server_path> <base_path>. Exiting.", instr);
+	process.exit(1);
+    }
+    return instr;
+}
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioHtmlStream = function(htmlstream) {
+    return cheerio.load(htmlstream);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(cheerioStream, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = cheerioStream(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
 };
 
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    return checkHtml($, checksfile);
+};
+
+var checkHtmlStream = function(fileUrl, checksfile, resultCallback) {
+    rest.get(fileUrl).on('complete', function(result, response) {
+        if (result instanceof Error) {
+            if (response)
+                console.error('Error: ' + util.format(response.message));
+            else
+                console.error('Error: no response from server');
+	} else {
+            $ = cheerioHtmlStream(result);
+            var checkJson = checkHtml($, checksfile);
+            resultCallback(checkJson);
+        }
+    });
+}
+
+var logResult = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u, --url [url]', 'Server path followed by the base url', assertUrlFormat, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	logResult(checkJson);
+    }
+    else if (program.url) {
+	checkHtmlStream(program.url.toString(), program.checks, logResult);
+    }
+    else {
+	console.log("No file or url specified, exit.");
+	process.exit(1);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlStream = checkHtmlStream;
 }
